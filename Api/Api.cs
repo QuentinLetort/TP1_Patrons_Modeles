d4c0aa4c2488532e3855bc87ce4e6a71a0d55d8a
camlemauff <camille.lemauff@hei.yncrea.fr>
2020-02-10 15:10:29 -0500

    api component

---
+++ Api/Api.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Api
+{
+    public class Api
+    {
+        private IDataFlow fluxMessage;
+        private int nbThread;
+        private bool listener;
+
+
+        public Api(IDataFlow flux, int nbThread, bool listener)
+        {
+            fluxMessage = flux;
+            nbThread = nbThread;
+            listener = listener;
+        }
+
+        public CreateConnection()
+        {
+            CommandExecutor.CommandExecutor(nbThread);
+        }
+
+        public chooseListener()
+        {
+            if(listener == true)
+            {
+                CommandExecutor.Subscribe();
+            }
+            else
+            {
+                CommandExecutor.Unsubscribe();
+            }
+        }
+
+        public CloseConnection()
+        {
+            CommandExecutor.Shutdown();
+        }
+    }
+}
